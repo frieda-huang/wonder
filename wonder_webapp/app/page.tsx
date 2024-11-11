297daad160cc4569b3f6967c146b1fc771099fb9
Frieda (Jingying) Huang <jingyingfhuang@gmail.com>
2024-11-11 14:33:00 -0500

    Added a picture

--- wonder_webapp/app/page.tsx
+++ wonder_webapp/app/page.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>home sweet home mars</div>;
+  return (
+    <div className="flex justify-center">
+      <Image
+        src="/images/youcanjustdothings.jpeg"
+        width={1000}
+        height={1000}
+        alt="You can just do things"
+        style={{ borderRadius: "15px" }}
+      />
+    </div>
+  );
 }
